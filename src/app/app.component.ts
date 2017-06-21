8b33f71a9e3464e8e39b2e7f895761d7f82177bd
Hifumisama <m.khayat92@gmail.com>
2017-06-21 16:54:55 +0200

    Le menu a été implémenté, cependant il manque peut être un petit peu de styling à faire :D

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { NavController, MenuController } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
+import { EspacesPage } from "../pages/espaces/espaces";
+import { TarifsPage } from "../pages/tarifs/tarifs";
+import { InfosPratiquesPage } from "../pages/infos-pratiques/infos-pratiques";
+import { ContactPage } from "../pages/contact/contact";
+
 @Component({
   templateUrl: 'app.html'
 })
+
 export class MyApp {
+  @ViewChild('mycontent') nav: NavController;
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    public menu: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +30,20 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+  linkEspaces(){
+    this.nav.push(EspacesPage);
+    this.menu.close();
+  }
+  linkTarifs(){
+    this.nav.push(TarifsPage);
+    this.menu.close();
+  }
+  linkInfosPratiques(){
+    this.nav.push(InfosPratiquesPage);
+    this.menu.close();
+  }
+  linkContact(){
+    this.nav.push(ContactPage);
+    this.menu.close();
+  }
 }
-
